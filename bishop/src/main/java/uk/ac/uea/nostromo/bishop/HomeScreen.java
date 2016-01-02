28725a47fd764650e58a01b1027952bacb435852
Barry Wright <wright.barry@gmx.com>
2016-01-02 01:07:07 +0000

    added two basic screens to load between and run simple template for both

---
+++ bishop/src/main/java/uk/ac/uea/nostromo/bishop/HomeScreen.java
@@ -0,0 +1,67 @@
+package uk.ac.uea.nostromo.bishop;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import uk.ac.uea.nostromo.mother.Game;
+import uk.ac.uea.nostromo.mother.Screen;
+
+/**
+ * Created by Barry on 02/01/2016.
+ */
+public class HomeScreen extends Screen {
+
+    HomeScreen(Game game, Context context){
+        super(game, context);
+
+        TextView title = new TextView(context);
+        title.setText("we are in first view");
+        title.setTextColor(Color.BLACK);
+        screenLayout.addView(title);
+
+        Button secondScreenButton = new Button(context);
+        secondScreenButton.setText("Goto Second Screen");
+        secondScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickSecondView(v);
+            }
+        });
+        screenLayout.addView(secondScreenButton);
+    }
+    public void onClickSecondView(View view) {
+        game.setScreen(new ParkingScreen(game, context));
+    }
+
+    @Override
+    public void update(float deltaTime) {
+
+    }
+
+    @Override
+    public void paint(float deltaTime) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public void backButton() {
+    }
+}
---
+++ bishop/src/main/java/uk/ac/uea/nostromo/bishop/MainActivity.java
@@ -0,0 +1,54 @@
+package uk.ac.uea.nostromo.bishop;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import uk.ac.uea.nostromo.mother.Screen;
+import uk.ac.uea.nostromo.mother.implementation.AndroidGame;
+
+
+public class MainActivity extends AndroidGame {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState, R.layout.activity_main, R.id.table_layout);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Screen getInitScreen() {
+        try {
+            Context context = getBaseContext();
+            return new HomeScreen(this, context);
+        }
+        catch (Exception ex){
+            String exs = ex.getMessage();
+            System.out.print(exs);
+        }
+        return null;
+    }
+}
